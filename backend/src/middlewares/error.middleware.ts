ad7389186a3c93678543922567187f4a81753798
Harshita <hariganti2003@gmail.com>
2026-03-03 11:04:12 +0530

    Backend: Middleware Implementation

---
+++ backend/src/middlewares/error.middleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "../utils/AppError";
+
+export const errorMiddleware = (
+  err: any,
+  req: Request,
+  res: Response,
+  _next: NextFunction
+) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+  console.error("Unexpected Error:", err);
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+};
